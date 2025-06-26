06bf4c882074e8e2ba7db1995b5707f0a789ae0e
sts <suitianshuang@gmail.com>
2025-06-26 23:02:02 +0800

    chore: ignore application-test.properties

---
+++ app/application/src/main/java/com/alipay/application/service/resource/DelResourceService.java
@@ -0,0 +1,12 @@
+package com.alipay.application.service.resource;
+
+
+/*
+ *@title DelResourceService
+ *@description
+ *@author suitianshuang
+ *@version 1.0
+ *@create 2025/6/26 22:17
+*/
+ public class DelResourceService {
+}
---
+++ app/application/src/main/java/com/alipay/application/service/resource/DelResourceServiceImpl.java
@@ -0,0 +1,12 @@
+package com.alipay.application.service.resource;
+
+
+/*
+ *@title DelResourceServiceImpl
+ *@description
+ *@author suitianshuang
+ *@version 1.0
+ *@create 2025/6/26 22:19
+*/
+ public class DelResourceServiceImpl {
+}
